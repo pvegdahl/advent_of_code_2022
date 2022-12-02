2473ad109811db2a94d366fe1eb39e0d92186b2d
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-01 20:05:10 -0500

    Day1A is coming along

--- lib/day01.ex
+++ lib/day01.ex
@@ -2,7 +2,7 @@ defmodule Day01 do
   def parse_input(path) do
     File.stream!(path, [:utf8])
     |> Stream.map(&String.trim/1)
-    |> Stream.map(&Day01.to_int_or_nil/1)
+    |> Stream.map(&to_int_or_nil/1)
     |> Enum.into([])
   end
 
@@ -14,9 +14,13 @@ defmodule Day01 do
   end
 
   def to_list_of_lists([], final_list), do: Enum.reverse(final_list)
-  def to_list_of_lists(["" | tail], list_so_far), do: to_list_of_lists(tail, [[], list_so_far])
   def to_list_of_lists(input_list, []), do: to_list_of_lists(input_list, [[]])
+  def to_list_of_lists([nil | tail], list_so_far), do: to_list_of_lists(tail, [[] | list_so_far])
   def to_list_of_lists([input_head | input_tail], [result_head | result_tail]) do
     to_list_of_lists(input_tail, [[input_head | result_head] | result_tail])
+  end
+
+  def part_a(path) do
+    nil
   end
 end
