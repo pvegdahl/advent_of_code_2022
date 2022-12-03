b47a973a4c40fa543002bed63b53b716f0a36716
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-03 16:22:01 -0500

    Create day 3 from template

---
+++ lib/day03.ex
@@ -0,0 +1,16 @@
+defmodule Day03 do
+  def parse_line(line) do
+    []
+  end
+
+  def part_a() do
+    # File.stream!("puzzle_input/day03.txt", [:utf8])
+  end
+
+  def part_b() do
+    # File.stream!("puzzle_input/day03.txt", [:utf8])
+  end
+end
+
+IO.puts("Part A: #{Day02.part_a()}")
+IO.puts("Part B: #{Day02.part_b()}")
---
+++ test/day03_text.exs
@@ -0,0 +1,8 @@
+defmodule Day03Test do
+  use ExUnit.Case
+
+  test "Tautology" do
+    assert 1 == 1
+  end
+
+end
