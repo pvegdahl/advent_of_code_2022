7461eccae9d9deafd482d91831f940b3161264bc
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-04 20:40:09 -0500

    Basic day 4 setup

---
+++ lib/day04.ex
@@ -0,0 +1,19 @@
+# Template
+defmodule DayXX do
+  def parse_line(line) do
+    []
+  end
+
+  def part_a() do
+    # File.stream!("puzzle_input/dayXX.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+
+  def part_b() do
+    # File.stream!("puzzle_input/dayXX.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+end
+
+# IO.puts("Part A: #{DayXX.part_a()}")
+# IO.puts("Part B: #{DayXX.part_b()}")
---
+++ test/day04_text.exs
@@ -0,0 +1,8 @@
+# Template
+defmodule DayXXTest do
+  use ExUnit.Case
+
+  test "Tautology" do
+    assert 1 == 1
+  end
+end
