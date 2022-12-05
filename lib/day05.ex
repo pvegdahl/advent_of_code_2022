a6aad2f454dad237b246813a6268b32909e82710
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-05 09:25:06 -0500

    Copy template for day 5

---
+++ lib/day05.ex
@@ -0,0 +1,18 @@
+defmodule Day05 do
+  def parse_line(line) do
+    []
+  end
+
+  def part_a() do
+    # File.stream!("puzzle_input/day05.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+
+  def part_b() do
+    # File.stream!("puzzle_input/day05.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+end
+
+# IO.puts("Part A: #{Day05.part_a()}")
+# IO.puts("Part B: #{Day05.part_b()}")
