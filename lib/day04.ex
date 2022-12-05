f0e16714c61e930005eeb4c901195455e2a2c117
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-04 20:55:59 -0500

    Parsing a line into ranges works

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,19 +1,21 @@
-# Template
-defmodule DayXX do
+defmodule Day04 do
   def parse_line(line) do
-    []
+    String.split(line, ",")
+    |> Stream.map(&(String.split(&1, "-")))
+    |> Enum.map(fn [first, last] -> String.to_integer(first)..String.to_integer(last) end)
+    |> List.to_tuple()
   end
 
   def part_a() do
-    # File.stream!("puzzle_input/dayXX.txt", [:utf8])
+    # File.stream!("puzzle_input/day04.txt", [:utf8])
     # |> Stream.map(&String.trim/1)
   end
 
   def part_b() do
-    # File.stream!("puzzle_input/dayXX.txt", [:utf8])
+    # File.stream!("puzzle_input/day04.txt", [:utf8])
     # |> Stream.map(&String.trim/1)
   end
 end
 
-# IO.puts("Part A: #{DayXX.part_a()}")
-# IO.puts("Part B: #{DayXX.part_b()}")
+# IO.puts("Part A: #{Day04.part_a()}")
+# IO.puts("Part B: #{Day04.part_b()}")
--- test/day04_text.exs
+++ test/day04_text.exs
@@ -1,8 +1,7 @@
-# Template
-defmodule DayXXTest do
+defmodule Day04Test do
   use ExUnit.Case
 
-  test "Tautology" do
-    assert 1 == 1
+  test "parse line into range pair" do
+    assert Day04.parse_line("2-4,6-8") == {2..4, 6..8}
   end
 end
