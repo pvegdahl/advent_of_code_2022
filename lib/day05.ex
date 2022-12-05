394014b3008d62a98de92e65c62419cd5c5edac3
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-05 09:47:05 -0500

    Okay, now we're really parsing a line of boxes

--- lib/day05.ex
+++ lib/day05.ex
@@ -4,9 +4,19 @@ defmodule Day05 do
   end
 
   def parse_box_line(line) do
-    String.split(line, " ")
-    |> Enum.map(&(String.at(&1, 1)))
+    split_line_into_boxes(line)
+    |> Enum.map(&whitespace_to_nil/1)
   end
+
+  def split_line_into_boxes(""), do: []
+
+  def split_line_into_boxes(line) do
+    {head, tail} = String.split_at(line, 4)
+    [String.at(head, 1) | split_line_into_boxes(tail)]
+  end
+
+  def whitespace_to_nil(" "), do: nil
+  def whitespace_to_nil(x), do: x
 
   def part_a() do
     # File.stream!("puzzle_input/day05.txt", [:utf8])
--- test/day05_test.exs
+++ test/day05_test.exs
@@ -4,4 +4,10 @@ defmodule Day05Test do
   test "Parse a simple line of boxes" do
     assert Day05.parse_box_line("[A] [B] [C]") == ~w(A B C)
   end
+
+  test "Parse a line of boxes with empties" do
+    assert Day05.parse_box_line("[A] [B]    ") == ["A", "B", nil]
+    assert Day05.parse_box_line("    [B] [C]") == [nil, "B", "C"]
+    assert Day05.parse_box_line("[A]     [C]") == ["A", nil, "C"]
+  end
 end
