7d1004f2641ed507734cfaf40e19fcf3345e92db
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-06 08:43:46 -0500

    Add puzzle input and template for day 6

---
+++ lib/day06.ex
@@ -0,0 +1,18 @@
+defmodule Day06 do
+  def parse_line(line) do
+    []
+  end
+
+  def part_a() do
+    # File.stream!("puzzle_input/day06.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+
+  def part_b() do
+    # File.stream!("puzzle_input/day06.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+end
+
+IO.puts("Part A: #{Day06.part_a()}")
+IO.puts("Part B: #{Day06.part_b()}")
