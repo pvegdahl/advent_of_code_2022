17505e1d6e6c24fa8e4ebedbbd98b446bed3d0b2
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-06 08:57:08 -0500

    Part A example input passes

--- test/day06_test.exs
+++ test/day06_test.exs
@@ -6,4 +6,12 @@ defmodule Day06Test do
     assert Day06.packet_begin_position("aacdefg") == 5
     assert Day06.packet_begin_position("abccefg") == 7
   end
+
+  test "Part A examples" do
+    assert Day06.packet_begin_position("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
+    assert Day06.packet_begin_position("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
+    assert Day06.packet_begin_position("nppdvjthqldpwncqszvftbrmjlhg") == 6
+    assert Day06.packet_begin_position("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
+    assert Day06.packet_begin_position("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+  end
 end
