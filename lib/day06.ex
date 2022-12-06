6979d7cdad2cbff6b340280bec1a3c2fc740fca2
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-06 09:02:23 -0500

    Refactor to allow passing in a specific number of required unique chars

--- lib/day06.ex
+++ lib/day06.ex
@@ -3,28 +3,28 @@ defmodule Day06 do
     []
   end
 
-  def packet_begin_position(packet) do
-    packet_begin_position(String.to_charlist(packet), 4)
+  def packet_begin_position(packet, unique_chars \\ 4) do
+    packet_begin_position(String.to_charlist(packet), unique_chars, unique_chars)
   end
 
-  defp packet_begin_position([_head | tail] = packet, position) do
-    distinct_chars = Enum.take(packet, 4)
+  defp packet_begin_position([_head | tail] = packet, position, unique_chars) do
+    Enum.take(packet, unique_chars)
     |> Enum.uniq()
     |> Enum.count()
-    |> check_packet_begin(tail, position)
+    |> check_packet_begin(tail, position, unique_chars)
   end
 
-  defp check_packet_begin(4, _packet_tail, position), do: position
+  defp check_packet_begin(unique_chars, _packet_tail, position, unique_chars), do: position
 
-  defp check_packet_begin(_distinct_chars, packet_tail, position) do
-    packet_begin_position(packet_tail, position+1)
+  defp check_packet_begin(_distinct_chars, packet_tail, position, unique_chars) do
+    packet_begin_position(packet_tail, position+1, unique_chars)
   end
 
   def part_a() do
     File.stream!("puzzle_input/day06.txt", [:utf8])
     |> Enum.map(&String.trim/1)
     |> List.first()
-    |> packet_begin_position()
+    |> packet_begin_position(4)
   end
 
   def part_b() do
