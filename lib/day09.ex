ece09c4bba8b8bfe0bbf253a8a30094a03a12841
Philip Vegdahl <pvegdahl@gmail.com>
2022-12-09 12:07:06 -0500

    Add day 9 template copy and puzzle input

---
+++ lib/day09.ex
@@ -0,0 +1,18 @@
+defmodule Day09 do
+  def parse_line(line) do
+    []
+  end
+
+  def part_a() do
+    # File.stream!("puzzle_input/day09.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+
+  def part_b() do
+    # File.stream!("puzzle_input/day09.txt", [:utf8])
+    # |> Stream.map(&String.trim/1)
+  end
+end
+
+IO.puts("Part A: #{Day09.part_a()}")
+IO.puts("Part B: #{Day09.part_b()}")
